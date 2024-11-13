f9b943a48c1c3691e503b753128c75daaa8f2159
milamilovic <milamilovic03@gmail.com>
2024-11-13 12:10:27 +0100

    marking simulators as offline

--- eps-nwt-backend/src/main/java/com/example/epsnwtbackend/service/RabbitMQListener.java
+++ eps-nwt-backend/src/main/java/com/example/epsnwtbackend/service/RabbitMQListener.java
@@ -1,5 +1,6 @@
 package com.example.epsnwtbackend.service;
 
+import com.example.epsnwtbackend.configuration.HeartbeatMonitor;
 import com.example.epsnwtbackend.model.ConsumptionMessage;
 import com.example.epsnwtbackend.model.HeartbeatMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.amqp.core.Message;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class RabbitMQListener {
@@ -18,15 +21,18 @@ public class RabbitMQListener {
     @Autowired
     private InfluxService influxService;
 
+    @Autowired
+    HeartbeatMonitor heartbeatMonitor;
+
     @RabbitListener(id = "heartbeatExchange", concurrency = "2")
-    public void recieveHeartbeat(Message message) {
+    public void receiveHeartbeat(Message message) {
         String messageContent = new String(message.getBody());
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             HeartbeatMessage parsedMessage = objectMapper.readValue(messageContent, HeartbeatMessage.class);
             System.out.println("Parsed message object heartbeat: " + parsedMessage);
-            float value = parsedMessage.getStatus().equals("online") ? 1 : 0;
-            influxService.saveHeartbeat(parsedMessage.getId(), value, parsedMessage.getTimestamp(), new HashMap<>());
+            heartbeatMonitor.updateHeartbeat(parsedMessage.getId(), parsedMessage.getTimestamp().toInstant());
+            influxService.saveHeartbeat(parsedMessage.getId(), 1, parsedMessage.getTimestamp(), new HashMap<>());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -35,11 +41,10 @@ public class RabbitMQListener {
     @RabbitListener(id = "consumptionExchange", concurrency = "2")
     public void receiveConsumption(Message message) {
         String messageContent = new String(message.getBody());
-        System.out.println("municipality: " + message.getMessageProperties().getConsumerQueue().split("_queue_")[1]);
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             ConsumptionMessage parsedMessage = objectMapper.readValue(messageContent, ConsumptionMessage.class);
-            System.out.println("Parsed message object consumption: " + parsedMessage);
+            System.out.println("Parsed message object heartbeat: " + parsedMessage);
             Map<String, String> tags = Map.of(
                     "Municipality", message.getMessageProperties().getConsumerQueue().split("_queue_")[1]
             );
