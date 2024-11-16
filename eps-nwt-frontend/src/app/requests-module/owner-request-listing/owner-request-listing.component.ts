67b6c1ff5811d3630ca20470269725672c133208
matejicdunja <dunja2002matejic@gmail.com>
2024-11-16 17:57:56 +0100

    sort owner requests

--- eps-nwt-frontend/src/app/requests-module/owner-request-listing/owner-request-listing.component.ts
+++ eps-nwt-frontend/src/app/requests-module/owner-request-listing/owner-request-listing.component.ts
@@ -3,20 +3,23 @@ import {OwnerRequestCardComponent} from "../owner-request-card/owner-request-car
 import {AllRealEstateRequestsDto} from "../../model/all-real-estate-requests-dto";
 import {RealEstateRequestService} from "../../service/real-estate-request.service";
 import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-owner-request-listing',
   standalone: true,
-  imports: [
-    OwnerRequestCardComponent,
-    NgForOf
-  ],
+    imports: [
+        OwnerRequestCardComponent,
+        NgForOf,
+        FormsModule
+    ],
   templateUrl: './owner-request-listing.component.html',
   styleUrl: './owner-request-listing.component.css'
 })
 export class OwnerRequestListingComponent implements OnInit{
   requests: AllRealEstateRequestsDto[] = [];
   loggedInId : number = 0;
+  sortType: string = "createdAtAsc";
 
   constructor(private service: RealEstateRequestService) {}
 
@@ -31,5 +34,36 @@ export class OwnerRequestListingComponent implements OnInit{
         console.log("Error with all requests for owner " + this.loggedInId + " !")
       }
     });
+  }
+
+  getTime(date: Date | null) {
+    return date != null ? new Date(date).getTime() : 0;
+  }
+
+  sortRequests(): void {
+    switch (this.sortType) {
+      case 'createdAtAsc':
+        console.log("usao u rastuce")
+        this.requests = this.requests.sort((a, b) => {
+            return this.getTime(a.createdAt) - this.getTime(b.createdAt);
+        });
+        break;
+      case 'createdAtDesc':
+          console.log("usao u opadajuce")
+          this.requests = this.requests.sort((a, b) => {
+              return this.getTime(b.createdAt) - this.getTime(a.createdAt);
+          });
+        break;
+      case 'approvedAtAsc':
+          this.requests = this.requests.sort((a, b) => {
+              return this.getTime(a.approvedAt) - this.getTime(b.approvedAt);
+          });
+        break;
+      case 'approvedAtDesc':
+          this.requests = this.requests.sort((a, b) => {
+              return this.getTime(b.approvedAt) - this.getTime(a.approvedAt);
+          });
+        break;
+    }
   }
 }
