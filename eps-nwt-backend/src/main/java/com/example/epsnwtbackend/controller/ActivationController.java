27b60afb819a0c966106087f88eb477d0beff200
natasa-dz <dzudzar.n@gmail.com>
2024-11-19 13:33:58 +0100

    - Modified WebSecurityConfig in order to enable activation.

--- eps-nwt-backend/src/main/java/com/example/epsnwtbackend/controller/ActivationController.java
+++ eps-nwt-backend/src/main/java/com/example/epsnwtbackend/controller/ActivationController.java
@@ -1,5 +1,6 @@
 package com.example.epsnwtbackend.controller;
-
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.epsnwtbackend.model.User;
 import com.example.epsnwtbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +9,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")  // Allow requests from your frontend
 @RequestMapping("/users/auth")
 public class ActivationController {
 
     @Autowired
     private UserRepository userRepository;
-
+    @PreAuthorize("permitAll()")
     @PatchMapping("/activate")
-    public ResponseEntity<String> activateAccount(@RequestParam String token) {
-        System.out.println("Token sa front-a: "+token);
+    public ResponseEntity<Void> activateAccount(@RequestParam String token) {
+        System.out.println("Token sa front-a: " + token);
         User user = userRepository.findByActivationToken(token);
-        System.out.println("Pronadjeni user: "+user.toString());
+        System.out.println("Pronadjeni user: " + user.toString());
         if (!user.isActive()) {
             user.setActive(true);
             user.setActivationToken(null);
-            System.out.println("User iz if-a: "+user.toString());
             userRepository.save(user);
             return ResponseEntity.ok().build();
         } else {
