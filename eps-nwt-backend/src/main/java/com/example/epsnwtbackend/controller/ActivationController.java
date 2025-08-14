bc92132bbdbf5cc98a4b87fa51ce30b76ba0e9da
milamilovic <milamilovic03@gmail.com>
2025-08-14 21:22:03 +0200

    added caching to the rest of the services

--- eps-nwt-backend/src/main/java/com/example/epsnwtbackend/controller/ActivationController.java
+++ eps-nwt-backend/src/main/java/com/example/epsnwtbackend/controller/ActivationController.java
@@ -3,9 +3,12 @@ package com.example.epsnwtbackend.controller;
 import com.example.epsnwtbackend.model.User;
 import com.example.epsnwtbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")  // Allow requests from your frontend
@@ -14,6 +17,9 @@ public class ActivationController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     @PatchMapping("/activate")
     public ResponseEntity<String> activateAccount(@RequestParam String token) {
@@ -25,6 +31,10 @@ public class ActivationController {
             user.setActivationToken(null);
             System.out.println("User iz if-a: "+user.toString());
             userRepository.save(user);
+            Objects.requireNonNull(cacheManager.getCache("userByUsername")).evict(user.getUsername());
+            Objects.requireNonNull(cacheManager.getCache("userByEmail")).evict(user.getUsername());
+            Objects.requireNonNull(cacheManager.getCache("userById")).evict(user.getId());
+            Objects.requireNonNull(cacheManager.getCache("userPhoto")).evict(user.getId());
             return ResponseEntity.ok("Account activated successfully!");
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid or expired activation token.");
--- eps-nwt-backend/src/main/java/com/example/epsnwtbackend/service/EmployeeService.java
+++ eps-nwt-backend/src/main/java/com/example/epsnwtbackend/service/EmployeeService.java
@@ -1,13 +1,20 @@
 package com.example.epsnwtbackend.service;
 
+import com.example.epsnwtbackend.dto.CacheablePage;
+import com.example.epsnwtbackend.dto.CitizenSearchDTO;
 import com.example.epsnwtbackend.dto.EmployeeSearchDTO;
+import com.example.epsnwtbackend.dto.ViewEmployeeDTO;
 import com.example.epsnwtbackend.model.Employee;
 import com.example.epsnwtbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,15 +24,23 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @Caching(evict = {
+            @CacheEvict(value = "employeeSearch", allEntries = true),
+            @CacheEvict(value = "allEmployees", allEntries = true),
+            @CacheEvict(value = "allEmployeesNoPagination", allEntries = true)
+    })
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
-    public Page<EmployeeSearchDTO> getAllEmployees(Pageable pageable) {
-        return employeeRepository.findAll(pageable)
-                .map(employee -> EmployeeSearchDTO.toDto(employee));
+    @Cacheable(value = "allEmployees", key = "{#pageable.pageNumber, #pageable.pageSize}")
+    public CacheablePage<EmployeeSearchDTO> getAllEmployees(Pageable pageable) {
+        Page<EmployeeSearchDTO> employees =   employeeRepository.findAll(pageable)
+                .map(EmployeeSearchDTO::toDto);
+        return new CacheablePage<EmployeeSearchDTO>(new ArrayList<>(employees.getContent()), employees.getTotalPages(), employees.getTotalElements());
     }
 
+    @Cacheable(value = "allEmployeesNoPagination")
     public List<EmployeeSearchDTO> getAllEmployeesNoPagination() {
         return employeeRepository.findAll()
                 .stream()
@@ -33,21 +48,29 @@ public class EmployeeService {
                 .collect(Collectors.toList());
     }
 
+    @Cacheable(value = "employeeDetails", key = "#id")
+    public ViewEmployeeDTO getEmployeeById(Long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        return employee.map(ViewEmployeeDTO::toDto).orElse(null);
+    }
 
-
-    public Optional<Employee> getEmployeeById(Long id) {
+    public Optional<Employee> getEmployeeEntity(Long id) {
         return employeeRepository.findById(id);
     }
 
-    public Page<EmployeeSearchDTO> search(String username, Pageable pageable) {
+    @Cacheable(value = "employeeSearch", key = "{#username, #pageable.pageNumber, #pageable.pageSize}")
+    public CacheablePage<EmployeeSearchDTO> search(String username, Pageable pageable) {
         if (username == null || username.trim().isEmpty()) {
-            return employeeRepository.findAll(pageable).map(
+            Page<EmployeeSearchDTO> employees =  employeeRepository.findAll(pageable).map(
                     e -> new EmployeeSearchDTO(e.getId(), e.getUser().getUsername(), e.getName(), e.getSurname())
             );
+            return new CacheablePage<EmployeeSearchDTO>(new ArrayList<>(employees.getContent()), employees.getTotalPages(), employees.getTotalElements());
         }
-        return employeeRepository.findAllWithUsername(username, pageable);
+        Page<EmployeeSearchDTO> employees =   employeeRepository.findAllWithUsername(username, pageable);
+        return new CacheablePage<EmployeeSearchDTO>(new ArrayList<>(employees.getContent()), employees.getTotalPages(), employees.getTotalElements());
     }
 
+    @Cacheable(value = "employeeDetailsUserId", key = "#id")
     public Employee getEmployeeByUserId(Long id) {
         return employeeRepository.findByUserId(id);
     }
